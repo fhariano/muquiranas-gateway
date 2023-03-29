7f7310966bb3744ab8e4fce7d18b12ed01efa254
Flavio Ariano <fhariano@gmail.com>
2023-03-29 19:22:37 -0300

    Ajustes CORS with Laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -105,6 +105,12 @@ $app->routeMiddleware([
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Fruitcake\Cors\CorsServiceProvider::class);
+$app->configure('cors');
+
+$app->middleware([
+    Fruitcake\Cors\HandleCors::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
